d0c14a27e262101fa74dbc3ba5ee4240e585dcf2
Priya <priyaa.jass@gmail.com>
2021-01-20 19:33:48 +0800

    added animations

--- app/src/main/java/com/example/fitstopwatch/MainActivity.kt
+++ app/src/main/java/com/example/fitstopwatch/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.fitstopwatch
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var text1: TextView
     private lateinit var text2: TextView
     private lateinit var btn: Button
+    private lateinit var atg :Animation
+    private lateinit var txt :Animation
+    private lateinit var abtn :Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         text1=findViewById(R.id.text1)
         text2=findViewById(R.id.text2)
         btn=findViewById(R.id.getstart)
+        //loading animation
+        atg=AnimationUtils.loadAnimation(this,R.anim.atg)
+        txt=AnimationUtils.loadAnimation(this,R.anim.txt)
+        abtn=AnimationUtils.loadAnimation(this,R.anim.btn)
+
+
+        //passing animation
+        ivSplash.startAnimation(atg)
+        text1.startAnimation(txt)
+        text2.startAnimation(txt)
+        btn.startAnimation(abtn)
 
         //import font
         val Mlight = Typeface.createFromAsset(assets, "MLight.ttf")
